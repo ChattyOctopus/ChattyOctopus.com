0318f07491fa63380295fb2d49b4e97ef7c0a698
Jeff McBride <jeff@chattyoctopus.com>
2022-10-19 23:17:57 -0600

    Updated Google Analytics from UA to GA4

--- js/googleanalytics.js
+++ js/googleanalytics.js
@@ -1,9 +1,17 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-28866863-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+Promise.all([
+    new Promise((resolve) => {
+        var script = document.createElement('script');
+        script.src = "https://www.googletagmanager.com/gtag/js?id=G-PBG6GRZBM1";
+        script.async = true;
+        document.body.appendChild(script);
+        resolve();
+    }), 
+    new Promise((resolve) => {
+        gtag('js', new Date());
+      
+        gtag('config', 'G-PBG6GRZBM1');
+        resolve();
+    })
+]);
